0679f592d3d7cd66a7de96c81bd0cf73b70740b9
salomao <henriquersalomao@gmail.com>
2024-03-14 17:01:58 -0300

    Another fix on script

--- scripts/scriptSendMail.js
+++ scripts/scriptSendMail.js
@@ -1,23 +1,30 @@
-const nome = document.getElementById('nome').value;
-const cargo = document.getElementById('cargo').value;
-const mail = document.getElementById('email').value;
-const curso = document.getElementById('curso').value;
-const motivo = document.getElementById('motivo').value;
+document.getElementById('emailButton').addEventListener('click', sendMail);
 
-if(nome && cargo && mail && curso && motivo) {
-  Email.send({
-    Host : "smtp.yourisp.com",
-    Username : "enviaremailmatilhaca@gmail.com",
-    Password : "26430274431CA893425C09EC80395BCF6221",
-    To : 'matilhaca@gmail.com',
-    From : "enviaremailmatilhaca@gmail.com",
-    Subject : "INSCRIÇÃO PARA DIRETORIA",
-    Body : `Nome: ${nome} \n cargo: ${cargo} \n email: ${mail} \n 
-            curso: ${curso} \n motivo: ${motivo} `,
-  }).then(() => {
-    alert('Inscrição realizada com sucesso! Aguarde que entraremos em contato.');
-    location.reload();
-  });
-} else {
-  alert('Preencha todos os campos!');
+function sendMail(e) {
+  e.preventDefault();
+
+  const nome = document.getElementById('nome').value;
+  const cargo = document.getElementById('cargo').value;
+  const mail = document.getElementById('email').value;
+  const curso = document.getElementById('curso').value;
+  const motivo = document.getElementById('motivo').value;
+
+  if(nome && cargo && mail && curso && motivo) {
+    Email.send({
+      Host : "smtp.yourisp.com",
+      Username : "enviaremailmatilhaca@gmail.com",
+      Password : "26430274431CA893425C09EC80395BCF6221",
+      To : 'matilhaca@gmail.com',
+      From : "enviaremailmatilhaca@gmail.com",
+      Subject : "INSCRIÇÃO PARA DIRETORIA",
+      Body : `Nome: ${nome} \n cargo: ${cargo} \n email: ${mail} \n 
+              curso: ${curso} \n motivo: ${motivo} `,
+    }).then(() => {
+      alert('Inscrição realizada com sucesso! Aguarde que entraremos em contato.');
+      location.reload();
+    });
+  } else {
+    alert('Preencha todos os campos!');
+  }
+
 }
